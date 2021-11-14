31dbd238e6b4ba5f69e478689d994744c104e855
ViktorKorkishko <korkishko.viktor2001@gmail.com>
2021-11-14 20:23:33 +0200

    [added] deprecated pathes for second player

--- Quoridor.Model/Quoridor.cs
+++ Quoridor.Model/Quoridor.cs
@@ -228,16 +228,15 @@ namespace Quoridor
 
             Vector2 firstPlayerPosition = FirstPlayer.Position;
 
-            // Cell currentCell = Field.Cells[firstPlayerPosition.x, firstPlayerPosition.y];
             Cell currentCell = new Cell(Field, firstPlayerPosition);
             
-            List<Vector2> positionsThatPlayerCanReach = new List<Vector2>();
+            List<Vector2> positionsThatFirstPlayerCanReach = new List<Vector2>();
 
-            positionsThatPlayerCanReach.Add(currentCell.Position);
+            positionsThatFirstPlayerCanReach.Add(currentCell.Position);
 
-            for (int i = 0; i <= positionsThatPlayerCanReach.ToList().Count; i++)
+            for (int i = 0; i <= positionsThatFirstPlayerCanReach.ToList().Count; i++)
             {
-                currentCell.Position = positionsThatPlayerCanReach[i];
+                currentCell.Position = positionsThatFirstPlayerCanReach[i];
 
                 var upperPoint = currentCell.UpperCell;
                 if (upperPoint != null)
@@ -245,9 +244,9 @@ namespace Quoridor
                     var deprecatedPath = new DeprecatedPath(currentCell.Position, upperPoint.Position);
                     if (!DoesDeprecatedPathExist(deprecatedPath))
                     {
-                        if (!positionsThatPlayerCanReach.Contains(upperPoint.Position))
+                        if (!positionsThatFirstPlayerCanReach.Contains(upperPoint.Position))
                         {
-                            positionsThatPlayerCanReach.Add(upperPoint.Position);
+                            positionsThatFirstPlayerCanReach.Add(upperPoint.Position);
 
                             if (positionsThatHaveToBeReachableForFirstPlayer.Contains(upperPoint.Position))
                             {
@@ -263,9 +262,9 @@ namespace Quoridor
                     var deprecatedPath = new DeprecatedPath(currentCell.Position, lowerPoint.Position);
                     if (!DoesDeprecatedPathExist(deprecatedPath))
                     {
-                        if (!positionsThatPlayerCanReach.Contains(lowerPoint.Position))
+                        if (!positionsThatFirstPlayerCanReach.Contains(lowerPoint.Position))
                         {
-                            positionsThatPlayerCanReach.Add(lowerPoint.Position);
+                            positionsThatFirstPlayerCanReach.Add(lowerPoint.Position);
 
                             if (positionsThatHaveToBeReachableForFirstPlayer.Contains(lowerPoint.Position))
                             {
@@ -281,9 +280,9 @@ namespace Quoridor
                     var deprecatedPath = new DeprecatedPath(currentCell.Position, leftPoint.Position);
                     if (!DoesDeprecatedPathExist(deprecatedPath))
                     {
-                        if (!positionsThatPlayerCanReach.Contains(leftPoint.Position))
+                        if (!positionsThatFirstPlayerCanReach.Contains(leftPoint.Position))
                         {
-                            positionsThatPlayerCanReach.Add(leftPoint.Position);
+                            positionsThatFirstPlayerCanReach.Add(leftPoint.Position);
 
                             if (positionsThatHaveToBeReachableForFirstPlayer.Contains(leftPoint.Position))
                             {
@@ -299,9 +298,9 @@ namespace Quoridor
                     var deprecatedPath = new DeprecatedPath(currentCell.Position, rightPoint.Position);
                     if (!DoesDeprecatedPathExist(deprecatedPath))
                     {
-                        if (!positionsThatPlayerCanReach.Contains(rightPoint.Position))
+                        if (!positionsThatFirstPlayerCanReach.Contains(rightPoint.Position))
                         {
-                            positionsThatPlayerCanReach.Add(rightPoint.Position);
+                            positionsThatFirstPlayerCanReach.Add(rightPoint.Position);
 
                             if (positionsThatHaveToBeReachableForFirstPlayer.Contains(rightPoint.Position))
                             {
@@ -311,7 +310,103 @@ namespace Quoridor
                     }
                 }
 
-                if (currentCell.Position == positionsThatPlayerCanReach[positionsThatPlayerCanReach.Count - 1])
+                if (currentCell.Position == positionsThatFirstPlayerCanReach[positionsThatFirstPlayerCanReach.Count - 1])
+                {
+                    return true;
+                }
+            }
+            
+            List<Vector2> positionsThatHaveToBeReachableForSecondPlayer = new List<Vector2>();
+            for (int y = 0; y < fieldSize.y; y++)
+            {
+                positionsThatHaveToBeReachableForSecondPlayer.Add(Field.Cells[fieldSize.x - 1, y].Position);
+            }
+            
+            Vector2 secondPlayerPosition = FirstPlayer.Position;
+
+            currentCell = new Cell(Field, secondPlayerPosition);
+            
+            List<Vector2> positionsThatSecondPlayerCanReach = new List<Vector2>();
+
+            positionsThatSecondPlayerCanReach.Add(currentCell.Position);
+            
+            for (int i = 0; i <= positionsThatSecondPlayerCanReach.ToList().Count; i++)
+            {
+                currentCell.Position = positionsThatSecondPlayerCanReach[i];
+
+                var upperPoint = currentCell.UpperCell;
+                if (upperPoint != null)
+                {
+                    var deprecatedPath = new DeprecatedPath(currentCell.Position, upperPoint.Position);
+                    if (!DoesDeprecatedPathExist(deprecatedPath))
+                    {
+                        if (!positionsThatSecondPlayerCanReach.Contains(upperPoint.Position))
+                        {
+                            positionsThatSecondPlayerCanReach.Add(upperPoint.Position);
+
+                            if (positionsThatHaveToBeReachableForSecondPlayer.Contains(upperPoint.Position))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                var lowerPoint = currentCell.LowerCell;
+                if (lowerPoint != null)
+                {
+                    var deprecatedPath = new DeprecatedPath(currentCell.Position, lowerPoint.Position);
+                    if (!DoesDeprecatedPathExist(deprecatedPath))
+                    {
+                        if (!positionsThatSecondPlayerCanReach.Contains(lowerPoint.Position))
+                        {
+                            positionsThatSecondPlayerCanReach.Add(lowerPoint.Position);
+
+                            if (positionsThatHaveToBeReachableForSecondPlayer.Contains(lowerPoint.Position))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                var leftPoint = currentCell.LeftCell;
+                if (leftPoint != null)
+                {
+                    var deprecatedPath = new DeprecatedPath(currentCell.Position, leftPoint.Position);
+                    if (!DoesDeprecatedPathExist(deprecatedPath))
+                    {
+                        if (!positionsThatSecondPlayerCanReach.Contains(leftPoint.Position))
+                        {
+                            positionsThatSecondPlayerCanReach.Add(leftPoint.Position);
+
+                            if (positionsThatHaveToBeReachableForSecondPlayer.Contains(leftPoint.Position))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                var rightPoint = currentCell.RightCell;
+                if (rightPoint != null)
+                {
+                    var deprecatedPath = new DeprecatedPath(currentCell.Position, rightPoint.Position);
+                    if (!DoesDeprecatedPathExist(deprecatedPath))
+                    {
+                        if (!positionsThatSecondPlayerCanReach.Contains(rightPoint.Position))
+                        {
+                            positionsThatSecondPlayerCanReach.Add(rightPoint.Position);
+
+                            if (positionsThatHaveToBeReachableForSecondPlayer.Contains(rightPoint.Position))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                if (currentCell.Position == positionsThatSecondPlayerCanReach[positionsThatSecondPlayerCanReach.Count - 1])
                 {
                     return true;
                 }
